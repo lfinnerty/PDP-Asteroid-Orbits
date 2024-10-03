04c80ff747f437bc070b4b5a64a4eaefbb47607d
lfinnerty <finnerty.luke@gmail.com>
2024-10-03 16:32:45 -0700

    adding image injection function

--- pdp_asteroids/position_to_orbit.py
+++ pdp_asteroids/position_to_orbit.py
@@ -5,6 +5,8 @@ from astropy.time import Time
 import json
 from pymultinest.solve import solve
 import corner
+from glob import glob
+from astropy.io import fits
 
 def xy_to_rthet(x, y):
     r = np.sqrt(x**2 + y**2)
@@ -47,6 +49,7 @@ def parallax_to_dist(tp1, e1, p2, e2, dt):
     dist = baseline/np.sin(parallax/206265)
 
     ### Calculate distance error
+    ### Note that the WCS has intrinsic error values we can use
     # dist_err = 
 
     return dist, dist_err
@@ -66,7 +69,7 @@ def dist_to_parallax(time, r, theta, dt):
 
 def ra_dist_to_r_theta(time, ra, rp):
     ### FIXME do this vector addition based, it's easier 
-    heta_earth = 2*np.pi/365.25 *(time-2460392.400856)
+    theta_earth = 2*np.pi/365.25 *(time-2460392.400856)
     r_earth = 1
     xe, ye = rthet_to_xy(r_earth, theta_earth)
 
@@ -78,8 +81,69 @@ def ra_dist_to_r_theta(time, ra, rp):
 
     return r, rerr, theta, theta_err
 
-def inject_asteroid(image, time, parallax):
-    pass
+def Gauss2D(x, y, amp, x0,y0,sigx,sigy,rot=0):
+    a = np.cos(rot)**2/(2*sigx**2)+np.sin(rot)**2/(2*sigy**2)
+    b = np.sin(2*rot)/(2*sigx**2)-np.sin(2*rot)/(2*sigy**2)
+    c = np.sin(rot)**2/(2*sigx**2)+np.cos(rot)**2/(2*sigy**2)
+
+    return amp*np.exp(-a*(x-x0)**2 - b*(x-x0)*(y-y0) - c*(y-y0)**2)
+
+
+def inject_asteroid(hdulst, parallax, obsdate,obsdelta, fwhm=3.5, fluxlevel=20,noiselevel=50):
+    ### Decide where to add inital PSF
+    data = hdulst[0].data
+    data[np.isnan(data)] = 3.
+    header = hdulst[0].header
+    # plt.imshow(data,vmin=np.nanpercentile(data,5),vmax=np.nanpercentile(data,95))
+    # plt.show()
+
+    ### Get coordinate info in degrees
+    ra0, dec0 = header['CRVAL1'], header['CRVAL2']
+    dra, ddec = header['CD2_2'], header['CD1_1']
+    x0, y0 = header['CRPIX2'], header['CRPIX1']
+
+    ### Decide where to add the first PSF
+    x1, y1 = np.random.randint(200, data.shape[1]-200), np.random.randint(200, data.shape[0]-200)
+    ### Decide where to add the second PSF, making sure the offset gives the right parallax
+    d2 = (parallax/3600.)**2
+    ### Make sure we generate values in the image, away from edge effects
+    # print(x1, y1, np.sqrt(d2)/dra)
+    x2, y2 = -100, -100
+    while x2 < 100 or y2 < 100 or x2 > 900 or y2 > 900:
+        dx = np.random.uniform(low=-np.sqrt(d2),high=np.sqrt(d2))
+        dy = np.sqrt(d2-dx**2)
+        ### Randomize y offset sign
+        if np.random.uniform(0,1) > 0.5: dy*=-1
+        ### Convert to pixels and apply offset
+        x2, y2 = x1+dx/dra, y1+dy/ddec
+    # print(x1,x2, y1,y2)
+
+    ### Now we actually add the PSFs to the images
+    x, y = np.meshgrid(np.arange(data.shape[0]),np.arange(data.shape[1]))
+    rot = np.random.uniform(0,2*np.pi)
+    sigx, sigy = np.random.normal(fwhm,0.5), np.random.normal(fwhm,0.5)
+    amp = np.nanpercentile(data,fluxlevel)
+    psf1 = Gauss2D(x,y, amp, x1,y1,sigx,sigy,rot)
+    psf2 = Gauss2D(x,y, amp, x2,y2,sigx,sigy,rot)
+
+    ### Add PSFs to image
+    im1 = data+psf1
+    im2 = data+psf2
+    ### Add photon noise
+    im1 += np.random.normal(np.sqrt(np.abs(im1))) + np.random.normal(loc=0,scale=noiselevel,size=im1.shape)
+    im2 += np.random.normal(np.sqrt(np.abs(im2))) + np.random.normal(loc=0,scale=noiselevel,size=im1.shape)
+
+    # plt.imshow(im1-im2,vmin=-np.nanpercentile(data,10),vmax=np.nanpercentile(data,10))
+    # plt.show()
+    
+    ### Write to disk
+    print(obsdate)
+    fits.writeto('injected_images/'+obsdate+'_frame1.fits', data=im1, header=header)
+    fits.writeto('injected_images/'+obsdate+'_frame2.fits', data=im2, header=header)
+
+
+    ### Return two image arrays
+    return im1, im2
 
 def prior_transform(u):
     x = np.array(u)
@@ -123,14 +187,21 @@ class logl():
 
 
 if __name__ == '__main__':
+    ### Image filenames
+    image_list = glob('imagedata/*.fits')
+    nimages = len(image_list)
+    print(image_list)
+
     ### True parameters
     p0, a, e, omega = 0.2, 1.2, 0.83, np.pi/2.
     period = 365.25*np.sqrt(a**3)
+
     print('Period [days]:', period)
+    
+
     ### Given a list of dates, predict observables
-    obsdates = ['2025-01-18', '2025-03-02', '2025-04-01']#, '2025-04-29', '2025-05-12', '2025-05-29', '2025-06-29']
-    # obsdates = ['2025-01-'+str(i).zfill(2) for i in range(1,32,2)]
-    #obsdates = obsdates + ['2025-02-'+str(i).zfill(2) for i in range(1,29,2)]
+    obsdates = ['2025-01-18', '2025-03-02', '2025-04-01', '2025-04-29', '2025-05-12']
+    obsdelta = 4/24. ### In days
 
     jds = []
     for date in obsdates:
@@ -139,17 +210,19 @@ if __name__ == '__main__':
     
     rs, thetas = make_orbit(jds, p0, a, e, omega)
 
-    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
     for i in range(len(rs)):
-        ax.scatter(2*np.pi/365.25 *(jds[i]-2460392.400856), 1)
-        ax.scatter(thetas[i], rs[i])
-        parallax = dist_to_parallax(jds[i], rs[i], thetas[i], 0.3)
-        print(obsdates[i])
-        print('Parallax:', parallax)
-        dtheta = 0.3*2*np.pi/365.25
+        # ax.scatter(2*np.pi/365.25 *(jds[i]-2460392.400856), 1)
+        # ax.scatter(thetas[i], rs[i])
+        parallax = dist_to_parallax(jds[i], rs[i], thetas[i], obsdelta)
+        dtheta = obsdelta*2*np.pi/365.25
         baseline = np.sin(dtheta/2)
-        print('Distance:', 206265*baseline/parallax)
-    plt.show()
+        print(obsdates[i], 'Parallax:', parallax, 'Distance:', 206265*baseline/parallax)
+
+        ### Pick an image
+        idx = np.random.randint(0,nimages)
+        hdulst = fits.open(image_list[idx])
+        im1, im2 = inject_asteroid(hdulst, parallax, obsdates[i], obsdelta)
+
 
 
     ## Add errors to rs, thetas
@@ -157,6 +230,8 @@ if __name__ == '__main__':
     ### We will know theta already from the orbital parameters
     rs_err = np.random.normal(0*np.ones_like(rs),3e-2)
     rs_fit=rs+rs_err
+
+    ### This one we specify
     thetas_err = np.random.normal(0*np.ones_like(thetas),1e-4)
     thetas_fit = thetas+thetas_err
 
@@ -181,3 +256,4 @@ if __name__ == '__main__':
 
 
 
+   
