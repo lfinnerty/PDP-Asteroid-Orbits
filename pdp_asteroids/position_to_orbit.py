759b6a343ac4e4017ddc1a54715a1d04b82bcd62
lfinnerty <finnerty.luke@gmail.com>
2024-10-21 20:44:51 -0700

    minor bugfix

--- pdp_asteroids/position_to_orbit.py
+++ pdp_asteroids/position_to_orbit.py
@@ -213,7 +213,7 @@ def run_fit_dynesty(jds, rs_fit, rs_err, thetas_fit, thetas_err):
     res = dsampler.results
     return res.samples_equal()
 
-def make_images(obsdate, jd, r, theta, delta,image_list, fwhm, fluxlevel,noiselevel, output_dir: Path=FILE_DIR,):
+def make_images(obsdate, jd, r, theta, delta,image_list, fwhm, fluxlevel,noiselevel, output_dir: Path=FILE_DIR):
     parallax = dist_to_parallax(jd, r, theta, delta)
     dtheta = delta*2*np.pi/365.25
     baseline = np.sin(dtheta/2)
@@ -222,7 +222,7 @@ def make_images(obsdate, jd, r, theta, delta,image_list, fwhm, fluxlevel,noisele
     nimages = len(image_list)
     idx = np.random.randint(0,nimages)
     hdulst = fits.open(image_list[idx])
-    im1, im2, f1, f2 = inject_asteroid(hdulst, parallax, obsdate, delta,  fwhm, fluxlevel,noiselevel, output_dir: Path=FILE_DIR,)
+    im1, im2, f1, f2 = inject_asteroid(hdulst, parallax, obsdate, delta,  fwhm, fluxlevel,noiselevel, output_dir: Path=FILE_DIR)
 
     return im1, im2, f1, f2
 
